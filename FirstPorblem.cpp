e780a7e2218d1e3841a3fd4c8fd824371724f3c1
Ahmed Elsayed <ahmedelsayedmahmoud94@gmail.com>
2025-10-14 20:14:56 +0300

    pushing

--- FirstPorblem.cpp
+++ FirstPorblem.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
+void PrintMultiplicationTable(){
+    cout << "\t\t\t\tMultiplication Table (1 to 10)\n\n";
+    for (int i = 1; i <= 10; i++){
+        cout << "\t\t" << i;
+    }
+    cout << "____________________________________________________________\n";
+    for (int i = 1; i <= 10;i ++){
+        cout << i << "\t" << "| ";
+        for (int j = 1; j <= 10; j++){
+            cout << i * j << "\t\t";
+        }
+    }
+}
 int main(){
-    
+    PrintMultiplicationTable();
     return 0;
 }
