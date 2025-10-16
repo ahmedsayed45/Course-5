e8f95fc97cbe633c11f8f830702e13415659cf5b
Ahmed Elsayed <ahmedelsayedmahmoud94@gmail.com>
2025-10-16 15:13:23 +0300

    worked correctly on linux

--- FirstProblem.cpp
+++ FirstProblem.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 using namespace std;
-void PrintMultiplicationTable(){
-    cout << "\t\t\t\tMultiplication Table (1 to 10)\n\n";
+void PrintTableHeader(){
+    cout << "\t\t\tMultiplication Table (1 to 10)\n\n";
     for (int i = 1; i <= 10; i++){
-        cout << "\t\t" << i;
+        cout << "\t" << i;
     }
-    cout << "____________________________________________________________\n";
+    cout << "\n____________________________________________________________________________________\n";
+}
+string ColumnSeparator(int i){
+    if (i < 10){
+        return "     | ";
+    }
+    else{
+        return "    | ";
+    }
+}
+void PrintMultiplicationTable(){
+    PrintTableHeader();
     for (int i = 1; i <= 10;i ++){
-        cout << i << "\t" << "| ";
+        cout << i << ColumnSeparator(i);
         for (int j = 1; j <= 10; j++){
-            cout << i * j << "\t\t";
+            cout << i * j << "\t";
         }
+        cout << endl;
     }
 }
 int main(){
