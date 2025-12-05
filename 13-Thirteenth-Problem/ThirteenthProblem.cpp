556648fe43f7a7498ffcd003137e51ad9fb2b902
Ahmed Elsayed <ahmedelsayedmahmoud94@gmail.com>
2025-12-05 11:21:19 +0200

    Thirteenth Problem Completed

---
+++ 13-Thirteenth-Problem/ThirteenthProblem.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int ReadPositiveNumber(string Message){
+    int Number = 0;
+    do {
+        cout << Message;
+        cin >> Number;
+    } while (Number <= 0);
+    return Number;
+}
+void PrintInvertedNumberPattern(int Number){
+    for (int i = 1; i <= Number; i++){
+        for (int j = 1; j <= i; j++){
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+int main(){
+    PrintInvertedNumberPattern(ReadPositiveNumber("Please, enter a positive number : "));
+    return 0;
+}
