d21a5094a2d9a4601676984ef7d4506321e96716
Ahmed Elsayed <ahmedelsayedmahmoud94@gmail.com>
2026-01-20 07:27:25 +0200

    Ninteenth Problem Completed

---
+++ 19-Ninteenth-Problem/NinteenthProblem.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int RandomNumber(int From, int To){
+    return rand() % (To - From + 1) + From;
+}
+int main(){
+    srand((unsigned)time(NULL));
+    cout << RandomNumber(1, 5) << endl;
+    cout << RandomNumber(1, 5) << endl;
+    cout << RandomNumber(1, 5) << endl;
+    return 0;
+}
